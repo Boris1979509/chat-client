bd38d7334d5bd01db8b7892697b99dc65e5262a4
Boris <bobabonanadz@mail.ru>
2021-12-09 19:38:37 +0300

    Create interceptor response errors

--- src/plugins/axios/interceptors.js
+++ src/plugins/axios/interceptors.js
@@ -1,3 +1,6 @@
+import store from '@/store'
+import router from '@/router'
+
 const setAuthToken = (config) => {
     const token = localStorage.getItem(import.meta.env.VITE_AUTH_TOKEN_KEY)
     config.headers['Authorization'] = `Bearer ${token}`
@@ -6,18 +9,18 @@ const setAuthToken = (config) => {
 
 export const myInterceptor = (axios) => {
     axios.interceptors.request.use(setAuthToken)
-    // axios.interceptors.response.use(
-    //     (response) => {
-    //         return response.data
-    //     },
-    //     async (error) => {
-    //         if (error.response.status === 401) {
-    //             store.dispatch('auth/logout')
-    //             await router.push({ name: 'auth.login' })
-    //         } else if (error.response.status === 404) {
-    //             await router.push({ name: 'notFound' })
-    //         }
-    //         return Promise.reject(error)
-    //     }
-    // )
+    axios.interceptors.response.use(
+        (response) => {
+            return response
+        },
+        async (error) => {
+            if (error.response.status === 401) {
+                await store.dispatch('auth/logout')
+                router.push({ name: 'login' })
+            } else if (error.response.status === 404) {
+                router.push({ name: 'notFound' })
+            }
+            return Promise.reject(error)
+        }
+    )
 }
