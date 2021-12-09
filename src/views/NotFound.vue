d5fe935df72a69238d618a3676a5e52e01d29d10
Boris <bobabonanadz@mail.ru>
2021-12-09 19:37:08 +0300

    Create view: Page not found

---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="h-screen flex items-center">
+        <div class="container">
+            <app-alert type="error">
+                {{ $t('NotFound') }}
+                <router-link to="/" class="underline">{{
+                    $t('Home')
+                }}</router-link>
+            </app-alert>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFound',
+}
+</script>
