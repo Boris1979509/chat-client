7d90992011752c867087a77a9844f09922024235
Boris <bobabonanadz@mail.ru>
2021-12-11 13:51:32 +0300

    Add emit close sidebar mobile

--- src/components/chat/ChatsList.vue
+++ src/components/chat/ChatsList.vue
@@ -1,45 +1,4 @@
 <template>
-    <!-- <div class="my-3 mx-3">
-        <div class="relative text-gray-600 focus-within:text-gray-400">
-            <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-                <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    class="w-6 h-6 text-gray-500"
-                >
-                    <path
-                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    ></path>
-                </svg>
-            </span>
-            <input
-                aria-placeholder="Busca tus amigos o contacta nuevos"
-                placeholder="Busca tus amigos"
-                class="
-                    py-2
-                    pl-10
-                    block
-                    w-full
-                    rounded
-                    bg-gray-100
-                    outline-none
-                    focus:text-gray-700
-                "
-                type="search"
-                name="search"
-                required
-                autocomplete="search"
-            />
-        </div>
-    </div> -->
-
-    <h2 class="p-2 text-gray-600 text-lg">
-        {{ $t('Public chats') }}
-    </h2>
     <template v-if="chatsList && currentUser">
         <div class="overflow-auto h-screen truncate">
             <chat-item
@@ -69,7 +28,8 @@ import listeners from '@/plugins/socket/listeners'
 
 export default {
     name: 'ChatsList',
-    setup() {
+    emits: ['close'],
+    setup(_, { emit }) {
         const route = useRoute()
         const socket = inject('socket')
         const chatsList = computed(
@@ -111,6 +71,7 @@ export default {
         /** Emit selected chat */
         const selectChat = async (chatId) => {
             if (chatId === selectedChatId.value) return
+            emit('close') // if open sidebar mobile
             await getChat(chatId)
             removeChatIdFromPool(chatId)
             router.push({ query: { chatId } })
