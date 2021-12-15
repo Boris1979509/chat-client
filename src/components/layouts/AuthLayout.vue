a495b50c59b6d589d18894cd2d45e0284de58b2f
Boris <bobabonanadz@mail.ru>
2021-12-15 21:12:20 +0300

    Importing Asset as URL

--- src/components/layouts/AuthLayout.vue
+++ src/components/layouts/AuthLayout.vue
@@ -2,17 +2,9 @@
     <div class="container mx-auto">
         <div class="flex justify-center items-center h-screen">
             <div
-                class="
-                    flex
-                    w-full
-                    lg:w-1/2
-                    overflow-hidden
-                    rounded-lg
-                    shadow
-                    ring-1 ring-gray-300 ring-inset
-                "
+                class="flex w-full lg:w-1/2 overflow-hidden rounded-lg shadow ring-1 ring-gray-300 ring-inset"
             >
-                <app-image src="src/assets/chat-bg.png" alt="App chat" />
+                <app-image :src="imgUrl" alt="App chat" />
                 <router-view />
             </div>
         </div>
@@ -22,6 +14,7 @@
 <script>
 import AppImage from '@/components/ui/AppImage.vue'
 import { useSetTitle } from '@/use/setTitle'
+import imgUrl from '@/assets/chat-bg.png'
 export default {
     name: 'AuthLayout',
     components: {
@@ -29,6 +22,7 @@ export default {
     },
     setup() {
         useSetTitle()
+        return { imgUrl }
     },
 }
 </script>
