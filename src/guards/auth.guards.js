e0dd7446b53bfa709f75f30c60f4718f8e9b0fab
Boris <bobabonanadz@mail.ru>
2022-03-22 15:43:01 +0300

    Add route guard first login page

--- src/guards/auth.guards.js
+++ src/guards/auth.guards.js
@@ -1,15 +1,32 @@
 import router from '@/router'
 
 export const authGuard = (store) => {
-    const authRoutes = ['login', 'signup', 'forgot']
+    const authRoutes = ['login', 'signup', 'forgot', 'firstLoginSettings']
+    const settingsRoute = ['firstLoginSettings']
     router.beforeEach((to, from, next) => {
-        /** If user is auth and route name login */
-        if (authRoutes.includes(to.name) && store.state.auth.isLoggedIn) {
+        /**
+         * Скрыть страницы
+         * для аутентифицированного пользователя
+         */
+        if (
+            authRoutes.includes(to.name) &&
+            store.state.auth.isLoggedIn &&
+            store.getters['user/isUserInfo']
+        ) {
             return next({ name: 'home' })
         }
         if (!authRoutes.includes(to.name) && !store.state.auth.isLoggedIn) {
             return next({ name: 'login' })
         }
+        /**
+         * Если осуществляется переход
+         * незарегистрированного пользователя
+         * на страницу /first-login
+         */
+        if (settingsRoute.includes(to.name) && !store.state.auth.isLoggedIn) {
+            return next({ name: 'login' })
+        }
+
         return next()
     })
 }
